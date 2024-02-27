0d49b57a88d63d9b5a8e1c9f691363133b73d9db
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-02-27 14:26:19 +0300

    Add project files.

---
+++ CoreMVCModelSending_2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCModelSending_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Eger bir View metodu bir string argüman ile cagrılıyorsa artık icinde cagrıldıgı Action isminde bir View sayfası aramaz sizin ona verdiginiz string argüman isminde bir View sayfası arar...Ancak bilmelisniz bir View'in bu şekilde kullanılması standart degildir...Cünkü biz View'ların Action isimleriyle denk düsmesini isteriz ki Projemiz icerisinde barındırılan kodlar temiz bir şekilde gözüksün...
+
+
+            //View metodu, icerisine bir argüman verilmeden cagrıldıgı zaman hangi Action icerisindeyse o Action ile aynı isimde View döndürme görevine sahiptir...Ancak bir View metoduna explicit bir şekilde (acıkca) string bir tipte argüman verilirse o zaman o isimde bir View'i döndürmeye calısır...
+            return View("Deneme");
+        }
+    }
+}
